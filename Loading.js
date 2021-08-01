177170ebc3cf4c95fb388394c47676ce58fbe5bf
GilPOP <wolfye@naver.com>
2021-08-02 07:03:29 +0900

    AM 7:00 gg;

---
+++ Loading.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Loading () {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>It's a lovely day.
+            </Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:"flex-end",
+        paddingHorizontal:30,
+        paddingVertical:90,
+        backgroundColor: "#FDF6AA"
+    },
+    text:{
+        color: "#2c2c2c",
+        fontSize:30
+    }
+});
